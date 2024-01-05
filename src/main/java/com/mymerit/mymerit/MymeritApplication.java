10179371af2e0a89e89680eb51e6f4f5e971fb60
Daniel Chojnicki <djchojnicki@gmail.com>
2024-01-05 20:26:48 +0100

    spring skeleton

---
+++ src/main/java/com/mymerit/mymerit/MymeritApplication.java
@@ -0,0 +1,13 @@
+package com.mymerit.mymerit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymeritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymeritApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymerit/mymerit/MymeritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymerit.mymerit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymeritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
