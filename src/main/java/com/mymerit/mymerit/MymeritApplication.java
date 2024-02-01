971d29fc0a6d128ca60ad9e4ee0e8b27f4870b15
adamkujawski02 <102322536+adamkujawski02@users.noreply.github.com>
2024-02-01 17:08:44 +0100

    java-mail

--- src/main/java/com/mymerit/mymerit/MymeritApplication.java
+++ src/main/java/com/mymerit/mymerit/MymeritApplication.java
@@ -1,20 +1,44 @@
 package com.mymerit.mymerit;
 
+<<<<<<< Updated upstream
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+=======
+import com.mymerit.mymerit.mail.MailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+
 
 @SpringBootApplication
 @RestController
 public class MymeritApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MymeritApplication.class, args);
-	}
+	@Autowired
+	private MailSenderService mailSenderService;
 
-	@GetMapping("/")
-	public String apiRoot() {
-		return "Hello World!";
-	}
-}
+
+	public static void main(String[] args) {
+
+
+
+		SpringApplication.run(MymeritApplication.class, args);
+
+
+		}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendEmail(){
+
+         //w configu trzeba konto dac
+		mailSenderService.sendEmail("");
+
+}}
+
+
---
+++ src/main/java/com/mymerit/mymerit/api/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.mymerit.mymerit.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping()
+    public String home() {
+        return "home page";
+    }
+
+    @GetMapping("/secured-resource")
+    public String secured() {
+        return "logged in";
+    }
+
+
+
+}
