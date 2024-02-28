e17e9e33e59401b7292a6909045f9f63f89e3e9f
ni7x <99970419+ni7x@users.noreply.github.com>
2024-02-28 19:49:17 +0100

    stderr: Int -> String

--- src/main/java/com/mymerit/mymerit/api/payload/request/JudgeTokenHandlerRequest.java
+++ src/main/java/com/mymerit/mymerit/api/payload/request/JudgeTokenHandlerRequest.java
@@ -14,10 +14,10 @@ public class JudgeTokenHandlerRequest implements Serializable {
     private String stdout;
     private String status_id;
     private int language_id;
-    private int stderr;
+    private String stderr;
     private String compile_output;
 
-    public JudgeTokenHandlerRequest(String stdout, String status_id, int language_id, int stderr, String compile_output) {
+    public JudgeTokenHandlerRequest(String stdout, String status_id, int language_id, String stderr, String compile_output) {
         this.stdout = stdout;
         this.status_id = status_id;
         this.language_id = language_id;
