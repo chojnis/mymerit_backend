9d033bd09ff21d4846381bd00eee5c15b3af0119
ni7x <99970419+ni7x@users.noreply.github.com>
2024-03-15 14:35:46 +0100

    poklikalem cos

--- src/main/java/com/mymerit/mymerit/api/payload/response/JudgeCompilationResponse.java
+++ src/main/java/com/mymerit/mymerit/api/payload/response/JudgeCompilationResponse.java
@@ -7,9 +7,13 @@ import java.io.Serializable;
 @Getter
 @Setter
 public class JudgeCompilationResponse implements Serializable {
-
+    @Getter
+    @Setter
+    static class Status{
+        public Integer id;
+        public String description;
+    }
     private String stdout;
-    private String status_id;
     private int language_id;
     private String stderr;
     private String compile_output;
@@ -17,7 +21,6 @@ public class JudgeCompilationResponse implements Serializable {
 
     public JudgeCompilationResponse(String stdout, String status_id, int language_id, String stderr, String compile_output, Status status) {
         this.stdout = stdout;
-        this.status_id = status_id;
         this.language_id = language_id;
         this.stderr = stderr;
         this.compile_output = compile_output;
--- src/main/java/com/mymerit/mymerit/domain/service/JudgeService.java
+++ src/main/java/com/mymerit/mymerit/domain/service/JudgeService.java
@@ -21,38 +21,24 @@ public class JudgeService {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        String requestBody;
-
-        requestBody  = "{\n" +
+        String requestBody = "{\n" +
                 "  \"additional_files\": \"" + judgeTokenRequest.getFileContentBase64() + "\",\n" +
                 "  \"language_id\": " + 89 + "\n" +
                 "}";
 
-
-        System.out.println(requestBody);
         HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
-
         ResponseEntity<JudgeTokenEntity> response = restTemplate.postForEntity(apiUrl, request, JudgeTokenEntity.class);
-
-        System.out.println(response.getBody().getToken());
 
         return response.getBody().getToken();
     }
 
 
     public JudgeCompilationResponse generateRequestResponse(String token){
-
         RestTemplate restTemplate = new RestTemplate();
         String apiUrl = "http://localhost:2358/submissions/"+token+"?base64_encoded=true";
-
-
         ResponseEntity<JudgeCompilationResponse> response = restTemplate.getForEntity(apiUrl, JudgeCompilationResponse.class);
-        System.out.println(response.getBody().getStatus_id());
-
         return response.getBody();
     }
-
-
 }
 
 
