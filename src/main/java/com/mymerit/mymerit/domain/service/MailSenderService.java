5e5afe3c028e8df295a2d2bc634165a399893b02
mcar <85767699+r00tens@users.noreply.github.com>
2024-03-27 22:47:40 +0100

    refactored

--- src/main/java/com/mymerit/mymerit/domain/service/MailSenderService.java
+++ src/main/java/com/mymerit/mymerit/domain/service/MailSenderService.java
@@ -1,6 +1,7 @@
 package com.mymerit.mymerit.domain.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,36 +11,31 @@ import java.util.regex.Pattern;
 
 @Service
 public class MailSenderService {
+    Logger logger = LoggerFactory.getLogger(MailSenderService.class);
 
+    private final JavaMailSender javaMailSender;
 
-    @Autowired
-    private JavaMailSender javaMailSender;
+    public MailSenderService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
 
+    public void sendEmail(String mailDestination) {
+        if (isEmailCorrect(mailDestination)) {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(mailDestination);
+            mailMessage.setText("Your verification code is :"  + generateVerificationCode()  );
+            mailMessage.setSubject("Verify your MyMerit account");
 
-    public void sendEmail(String mailDestination){
+            javaMailSender.send(mailMessage);
 
-
-
-      if(isEmailCorrect(mailDestination)) {
-
-          SimpleMailMessage mailMessage = new SimpleMailMessage();
-          mailMessage.setTo(mailDestination);
-          mailMessage.setText("Your verification code is :"  + generateVerificationCode()  );
-          mailMessage.setSubject("Verify your MyMerit account");
-
-          javaMailSender.send(mailMessage);
-
-
-          System.out.println("Mail has been sent succesfully");
-      }
-      else{
-          System.out.println("Incorrect email syntax");
-      }
-
+            logger.info("Mail has been sent successfully");
+        }
+        else {
+            logger.info("Incorrect email syntax");
+        }
     }
 
     private boolean isEmailCorrect(String email){
-
         String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
 
         Pattern pattern = Pattern.compile(emailRegex);
@@ -47,18 +43,12 @@ public class MailSenderService {
         Matcher matcher = pattern.matcher(email);
 
         return matcher.matches();
-
     }
 
     public Integer generateVerificationCode(){
-
         int min = 1000; // Minimum value of range
         int max = 9999; // Maximum value of range
 
-
-        int verificationCode = (int)Math.floor(Math.random() * (max - min + 1) + min);
-
-        return verificationCode;
+        return (int)Math.floor(Math.random() * (max - min + 1) + min);
     }
-
 }
