9f6a59d2808c8f28c32a4eed9a6abebe458701b2
Kajetan Popiel <kajetanpopiel@gmail.com>
2024-04-24 15:48:57 +0200

    added get endpoint for user task history

--- src/main/java/com/mymerit/mymerit/infrastructure/repository/TaskHistoryRepository.java
+++ src/main/java/com/mymerit/mymerit/infrastructure/repository/TaskHistoryRepository.java
@@ -1,6 +1,5 @@
 package com.mymerit.mymerit.infrastructure.repository;
 
-import com.mymerit.mymerit.domain.entity.Task;
 import com.mymerit.mymerit.domain.entity.TaskHistory;
 import com.mymerit.mymerit.domain.entity.User;
 
@@ -11,6 +10,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskHistoryRepository extends MongoRepository<Task, String> {
+public interface TaskHistoryRepository extends MongoRepository<TaskHistory, String> {
     Optional<List<TaskHistory>> findByUser(User user);
 }
