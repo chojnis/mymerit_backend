94954a3e688ce30e8d196a19ddede998834df257
mcar <85767699+r00tens@users.noreply.github.com>
2024-05-26 17:39:16 +0200

    minor refactoring

--- src/main/java/com/mymerit/mymerit/api/controller/UserController.java
+++ src/main/java/com/mymerit/mymerit/api/controller/UserController.java
@@ -1,27 +1,30 @@
 package com.mymerit.mymerit.api.controller;
 
+import com.mymerit.mymerit.api.payload.request.UpdateUserRequest;
 import com.mymerit.mymerit.api.payload.response.*;
 import com.mymerit.mymerit.domain.entity.*;
+import com.mymerit.mymerit.domain.exception.InsufficientCreditsException;
+import com.mymerit.mymerit.domain.exception.RewardNotFoundException;
+import com.mymerit.mymerit.domain.exception.UserNotFoundException;
 import com.mymerit.mymerit.domain.service.JobOfferService;
-import com.mymerit.mymerit.domain.service.MailSenderService;
+import com.mymerit.mymerit.domain.service.RewardService;
 import com.mymerit.mymerit.domain.service.UserDetailsImpl;
 import com.mymerit.mymerit.infrastructure.repository.*;
 import com.mymerit.mymerit.infrastructure.security.CurrentUser;
-import com.mymerit.mymerit.api.payload.request.UpdateUserRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.apache.commons.lang3.time.DateUtils;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -36,44 +39,37 @@ public class UserController {
     private final JobOfferRepository jobOfferRepository;
     private final BookmarkRepository bookmarkRepository;
 
+    private final RewardService rewardService;
 
-    private final RewardHistoryRepository rewardHistoryRepository;
-    private final RewardRepository rewardRepository;
-    private final MailSenderService mailSenderService;
-
-    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder, 
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder,
                           TaskHistoryRepository taskHistoryRepository, JobOfferHistoryRepository jobOfferHistoryRepository,
-                          RewardHistoryRepository rewardHistoryRepository, RewardRepository rewardRepository, MailSenderService mailSenderService, SolutionRepository solutionRepository, JobOfferRepository jobOfferRepository, BookmarkRepository bookmarkRepository) {
+                          SolutionRepository solutionRepository, JobOfferRepository jobOfferRepository,
+                          BookmarkRepository bookmarkRepository, RewardService rewardService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.taskHistoryRepository = taskHistoryRepository;
         this.jobOfferHistoryRepository = jobOfferHistoryRepository;
-        this.rewardHistoryRepository = rewardHistoryRepository;
-        this.rewardRepository = rewardRepository;
-        this.mailSenderService = mailSenderService;
         this.solutionRepository = solutionRepository;
         this.jobOfferRepository = jobOfferRepository;
         this.bookmarkRepository = bookmarkRepository;
+        this.rewardService = rewardService;
     }
-
 
     @Operation(summary = "Find user by their ID")
     @GetMapping("/user/{userId}")
     public ResponseEntity<?> getUserById(@PathVariable String userId) {
         Optional<User> userOptional = userRepository.findById(userId);
-        if (!userOptional.isPresent()) {
+        if (userOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
         }
 
         User user = userOptional.get();
 
 
-
         UserResponse userResponse = new UserResponse(user);
 
         return ResponseEntity.ok(userResponse);
     }
-
 
     @Operation(summary = "get current user")
     @GetMapping("/me")
@@ -84,55 +80,31 @@ public class UserController {
     }
 
     @Operation(summary = "purchase a reward")
-
-
     @PostMapping("/me/purchase/{idReward}")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<Reward> purchaseReward(@CurrentUser UserDetailsImpl userDetailsImpl, @PathVariable String idReward) {
-        User user = userRepository.findById(userDetailsImpl.getId())
-                .orElseThrow(() -> new RuntimeException("User " + userDetailsImpl.getId() + " not found"));
+        try {
+            Reward reward = rewardService.purchaseReward(userDetailsImpl.getId(), idReward);
 
-        Optional<Reward> reward = rewardRepository.findById(idReward);
-
-        if (reward.isEmpty()) {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.ok(reward);
+        } catch (UserNotFoundException | RewardNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (InsufficientCreditsException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-
-        if (user.getCredits() < reward.get().getCost()) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        user.setCredits(user.getCredits() - reward.get().getCost());
-        userRepository.save(user);
-
-        RewardHistory rewardHistory = new RewardHistory();
-        rewardHistory.setUser(user);
-        rewardHistory.setReward(reward.get());
-        rewardHistory.setDatePurchase(LocalDateTime.now());
-        rewardHistoryRepository.insert(rewardHistory);
-
-        mailSenderService.sendReward(reward.get().getName(), user.getEmail());
-
-        return ResponseEntity.ok(reward.get());
     }
 
-    @Operation( summary = "get current user reward history")
+    @Operation(summary = "get current user reward history")
     @GetMapping("/me/rewards")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<List<RewardHistoryResponse>> getCurrentUserRewards(@CurrentUser UserDetailsImpl userDetailsImpl) {
-        User user = userRepository.findById(userDetailsImpl.getId())
-                .orElseThrow(() -> new RuntimeException("User " + userDetailsImpl.getId() + " not found"));
+        try {
+            List<RewardHistoryResponse> rewardHistoryResponse = rewardService.getCurrentUserRewardHistory(userDetailsImpl.getId());
 
-        List<RewardHistory> userRewards = rewardHistoryRepository.findByUser(user)
-                .orElse(new ArrayList<>());
-
-        List<RewardHistoryResponse> rewardHistoryResponse = new ArrayList<>();
-
-        for (RewardHistory rewardHistory : userRewards) {
-            rewardHistoryResponse.add(new RewardHistoryResponse(rewardHistory.getReward(), rewardHistory.getDatePurchase()));
+            return ResponseEntity.ok(rewardHistoryResponse);
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-
-        return ResponseEntity.ok(rewardHistoryResponse);
     }
 
     @Operation(summary = "Lists all users")
@@ -145,68 +117,9 @@ public class UserController {
     @Operation(summary = "Updates user data")
     @PostMapping("/me/update")
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<?> updateUserProfileInfo(@CurrentUser UserDetailsImpl userDetailsImpl, @Valid @RequestBody UpdateUserRequest updateUserRequest){
-        Optional<User> user_check = userRepository.findById(userDetailsImpl.getId());
-        User user;
-        if( user_check != null){
-            user = user_check.get();
-        }
-        else{
-            return ResponseEntity.badRequest().body(new ApiResponse(false, "user doesnt exist"));
-        }
-        boolean changed = false;
-
-        if( updateUserRequest.getImageBase64() != null ){
-            user.setImageBase64(updateUserRequest.getImageBase64());
-            changed = true;
-        }
-        if( updateUserRequest.getDescription() != null ){
-            user.setDescription(updateUserRequest.getDescription());
-            changed = true;
-        }
-        if( updateUserRequest.getPassword() != null ){
-            user.setPassword(updateUserRequest.getPassword());
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            changed = true;
-        }
-        if( updateUserRequest.getUsername() != null ){
-            user.setUsername(updateUserRequest.getUsername());
-            changed = true;
-        }
-        //
-        if( updateUserRequest.getSocialLink1() != null ){
-            user.setSocialLink1(updateUserRequest.getSocialLink1());
-            changed = true;
-        }
-        if( updateUserRequest.getSocialName1() != null ){
-            user.setSocialName1(updateUserRequest.getSocialName1());
-            changed = true;
-        }
-        if( updateUserRequest.getSocialLink2() != null ){
-            user.setSocialLink2(updateUserRequest.getSocialLink2());
-            changed = true;
-        }
-        if( updateUserRequest.getSocialName2() != null ){
-            user.setSocialName2(updateUserRequest.getSocialName2());
-            changed = true;
-        }
-        if( updateUserRequest.getSocialLink3() != null ){
-            user.setSocialLink3(updateUserRequest.getSocialLink3());
-            changed = true;
-        }
-        if( updateUserRequest.getSocialName3() != null ){
-            user.setSocialName3(updateUserRequest.getSocialName3());
-            changed = true;
-        }
-        //
-        if( changed == true ){
-            userRepository.save(user);
-            return ResponseEntity.ok( ).body(new ApiResponse(true, "account data updated"));
-        }
-        return ResponseEntity.badRequest().body(new ApiResponse(false, "failed to update account data"));
+    public ResponseEntity<ApiResponse> updateUserProfileInfo(@CurrentUser UserDetailsImpl userDetailsImpl, @Valid @RequestBody UpdateUserRequest updateUserRequest) {
+        return updateUser(userDetailsImpl.getId(), updateUserRequest);
     }
-
-    
 
     @Operation(summary = "Lists users task history")
     @GetMapping("/me/mytasks")
@@ -227,7 +140,7 @@ public class UserController {
         return ResponseEntity.ok(taskHistoryResponse);
     }
 
-    @Operation( summary = "Lists users solutions")
+    @Operation(summary = "Lists users solutions")
     @GetMapping("/me/solutions")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<List<SolutionListResponse>> getCurrentUserSolutionHistory(@CurrentUser UserDetailsImpl userDetailsImpl) {
@@ -238,7 +151,7 @@ public class UserController {
                 .stream()
                 .sorted(Comparator.comparing(Solution::getSubmitDate).reversed())
                 .map(solution -> {
-                    boolean isFeedbackToday = solution.getFeedback() != null &&  solution.getFeedback().getSubmitDate().toLocalDate().isEqual(LocalDate.now());
+                    boolean isFeedbackToday = solution.getFeedback() != null && solution.getFeedback().getSubmitDate().toLocalDate().isEqual(LocalDate.now());
 
                     return new SolutionListResponse(
                             solution.getTask() != null ? solution.getTask().getJob().getId() : null,
@@ -257,7 +170,8 @@ public class UserController {
 
         return ResponseEntity.ok(solutionResponses);
     }
-    @Operation( summary = "Lists users bookmarks")
+
+    @Operation(summary = "Lists users bookmarks")
     @GetMapping("/me/bookmarks")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<List<JobOfferListResponse>> getCurrentUserBookmarks(@CurrentUser UserDetailsImpl userDetailsImpl) {
@@ -273,7 +187,7 @@ public class UserController {
         return ResponseEntity.ok(bookmarkedJobOffers);
     }
 
-    @Operation( summary = "Gets company user")
+    @Operation(summary = "Gets company user")
     @GetMapping("/company")
     @PreAuthorize("hasRole('COMPANY')")
     public User getCurrentCompanyUser(@CurrentUser UserDetailsImpl userDetailsImpl) {
@@ -281,72 +195,14 @@ public class UserController {
                 .orElseThrow(() -> new RuntimeException("Company user " + userDetailsImpl.getId() + " not found"));
     }
 
-    @Operation( summary = "Updates users data")
+    @Operation(summary = "Updates users data")
     @PostMapping("/company/update")
     @PreAuthorize("hasRole('COMPANY')")
-    public ResponseEntity<?> updateCompanyUserProfileInfo(@CurrentUser UserDetailsImpl userDetailsImpl, @Valid @RequestBody UpdateUserRequest updateUserRequest){
-        Optional<User> user_check = userRepository.findById(userDetailsImpl.getId());
-        User user;
-        if( !user_check.isPresent()){
-            user = user_check.get();
-        }
-        else{
-            return ResponseEntity.badRequest().body(new ApiResponse(false, "company user doesnt exist"));
-        }
-        boolean changed = false;
-
-        if( updateUserRequest.getImageBase64() != null ){
-            user.setImageBase64(updateUserRequest.getImageBase64());
-            changed = true;
-        }
-        if( updateUserRequest.getDescription() != null ){
-            user.setDescription(updateUserRequest.getDescription());
-            changed = true;
-        }
-        if( updateUserRequest.getPassword() != null ){
-            user.setPassword(updateUserRequest.getPassword());
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            changed = true;
-        }
-        if( updateUserRequest.getUsername() != null ){
-            user.setUsername(updateUserRequest.getUsername());
-            changed = true;
-        }
-        //
-        if( updateUserRequest.getSocialLink1() != null ){
-            user.setSocialLink1(updateUserRequest.getSocialLink1());
-            changed = true;
-        }
-        if( updateUserRequest.getSocialName1() != null ){
-            user.setSocialName1(updateUserRequest.getSocialName1());
-            changed = true;
-        }
-        if( updateUserRequest.getSocialLink2() != null ){
-            user.setSocialLink2(updateUserRequest.getSocialLink2());
-            changed = true;
-        }
-        if( updateUserRequest.getSocialName2() != null ){
-            user.setSocialName2(updateUserRequest.getSocialName2());
-            changed = true;
-        }
-        if( updateUserRequest.getSocialLink3() != null ){
-            user.setSocialLink3(updateUserRequest.getSocialLink3());
-            changed = true;
-        }
-        if( updateUserRequest.getSocialName3() != null ){
-            user.setSocialName3(updateUserRequest.getSocialName3());
-            changed = true;
-        }
-        //
-        if( changed == true ){
-            userRepository.save(user);
-            return ResponseEntity.ok( ).body(new ApiResponse(true, "account data updated"));
-        }
-        return ResponseEntity.badRequest().body(new ApiResponse(false, "failed to update account data"));
+    public ResponseEntity<ApiResponse> updateCompanyUserProfileInfo(@CurrentUser UserDetailsImpl userDetailsImpl, @Valid @RequestBody UpdateUserRequest updateUserRequest) {
+        return updateUser(userDetailsImpl.getId(), updateUserRequest);
     }
 
-    
-    @Operation( summary = "Lists company's job offer history")
+    @Operation(summary = "Lists company's job offer history")
     @GetMapping("/company/myjoboffers")
     @PreAuthorize("hasRole('COMPANY')")
     public ResponseEntity<List<JobOfferHistoryResponse>> getCurrentCompanyUserJobOfferHistory(@CurrentUser UserDetailsImpl userDetailsImpl) {
@@ -356,7 +212,7 @@ public class UserController {
         List<JobOfferHistory> userJobOffers = jobOfferHistoryRepository.findByUserId(user.getId())
                 .orElse(new ArrayList<>());
 
-        if (userJobOffers.isEmpty() ) {
+        if (userJobOffers.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
@@ -369,4 +225,79 @@ public class UserController {
         return ResponseEntity.ok(jobOfferHistoryResponse);
     }
 
+    private boolean updateUserFields(User user, UpdateUserRequest updateUserRequest) {
+        boolean changed = false;
+
+        if (updateUserRequest.getImageBase64() != null) {
+            user.setImageBase64(updateUserRequest.getImageBase64());
+            changed = true;
+        }
+
+        if (updateUserRequest.getDescription() != null) {
+            user.setDescription(updateUserRequest.getDescription());
+            changed = true;
+        }
+
+        if (updateUserRequest.getPassword() != null) {
+            user.setPassword(updateUserRequest.getPassword());
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            changed = true;
+        }
+
+        if (updateUserRequest.getUsername() != null) {
+            user.setUsername(updateUserRequest.getUsername());
+            changed = true;
+        }
+
+        if (updateUserRequest.getSocialLink1() != null) {
+            user.setSocialLink1(updateUserRequest.getSocialLink1());
+            changed = true;
+        }
+
+        if (updateUserRequest.getSocialName1() != null) {
+            user.setSocialName1(updateUserRequest.getSocialName1());
+            changed = true;
+        }
+
+        if (updateUserRequest.getSocialLink2() != null) {
+            user.setSocialLink2(updateUserRequest.getSocialLink2());
+            changed = true;
+        }
+
+        if (updateUserRequest.getSocialName2() != null) {
+            user.setSocialName2(updateUserRequest.getSocialName2());
+            changed = true;
+        }
+
+        if (updateUserRequest.getSocialLink3() != null) {
+            user.setSocialLink3(updateUserRequest.getSocialLink3());
+            changed = true;
+        }
+
+        if (updateUserRequest.getSocialName3() != null) {
+            user.setSocialName3(updateUserRequest.getSocialName3());
+            changed = true;
+        }
+
+        return changed;
+    }
+
+    private ResponseEntity<ApiResponse> updateUser(String userId, UpdateUserRequest updateUserRequest) {
+        Optional<User> userOptional = userRepository.findById(userId);
+
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.badRequest().body(new ApiResponse(false, "user doesn't exist"));
+        }
+
+        User user = userOptional.get();
+        boolean changed = updateUserFields(user, updateUserRequest);
+
+        if (changed) {
+            userRepository.save(user);
+
+            return ResponseEntity.ok().body(new ApiResponse(true, "account data updated"));
+        } else {
+            return ResponseEntity.badRequest().body(new ApiResponse(false, "failed to update account data"));
+        }
+    }
 }
---
+++ src/main/java/com/mymerit/mymerit/domain/service/RewardService.java
@@ -0,0 +1,89 @@
+package com.mymerit.mymerit.domain.service;
+
+import com.mymerit.mymerit.api.payload.response.RewardHistoryResponse;
+import com.mymerit.mymerit.domain.entity.Reward;
+import com.mymerit.mymerit.domain.entity.RewardHistory;
+import com.mymerit.mymerit.domain.entity.User;
+import com.mymerit.mymerit.domain.exception.InsufficientCreditsException;
+import com.mymerit.mymerit.domain.exception.RewardNotFoundException;
+import com.mymerit.mymerit.domain.exception.UserNotFoundException;
+import com.mymerit.mymerit.infrastructure.repository.RewardHistoryRepository;
+import com.mymerit.mymerit.infrastructure.repository.RewardRepository;
+import com.mymerit.mymerit.infrastructure.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Service
+public class RewardService {
+    private final UserRepository userRepository;
+    private final RewardRepository rewardRepository;
+    private final RewardHistoryRepository rewardHistoryRepository;
+    private final MailSenderService mailSenderService;
+
+    public RewardService(UserRepository userRepository, RewardRepository rewardRepository, RewardHistoryRepository rewardHistoryRepository, MailSenderService mailSenderService) {
+        this.userRepository = userRepository;
+        this.rewardRepository = rewardRepository;
+        this.rewardHistoryRepository = rewardHistoryRepository;
+        this.mailSenderService = mailSenderService;
+    }
+
+    public Reward purchaseReward(String userId, String rewardId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User " + userId + " not found"));
+
+        Reward reward = rewardRepository.findById(rewardId)
+                .orElseThrow(() -> new RewardNotFoundException("Reward " + rewardId + " not found"));
+
+        if (user.getCredits() < reward.getCost()) {
+            throw new InsufficientCreditsException("Insufficient credits for user " + userId);
+        }
+
+        user.setCredits(user.getCredits() - reward.getCost());
+        userRepository.save(user);
+
+        RewardHistory rewardHistory = new RewardHistory();
+        rewardHistory.setUser(user);
+        rewardHistory.setReward(reward);
+        rewardHistory.setDatePurchase(LocalDateTime.now());
+        rewardHistoryRepository.save(rewardHistory);
+
+        String productKey = generateKey();
+        mailSenderService.sendReward(user, reward, productKey);
+
+        return reward;
+    }
+
+    public List<RewardHistoryResponse> getCurrentUserRewardHistory(String userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User " + userId + " not found"));
+
+        List<RewardHistory> userRewards = rewardHistoryRepository.findByUser(user)
+                .orElse(Collections.emptyList());
+
+        return userRewards.stream()
+                .map(rewardHistory -> new RewardHistoryResponse(rewardHistory.getReward(), rewardHistory.getDatePurchase()))
+                .collect(Collectors.toList());
+    }
+
+    private String generateKey() {
+        StringBuilder keyBuilder = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                keyBuilder.append((char) ('A' + random.nextInt(26)));
+            }
+
+            if (i < 3) {
+                keyBuilder.append("-");
+            }
+        }
+
+        return keyBuilder.toString();
+    }
+}
