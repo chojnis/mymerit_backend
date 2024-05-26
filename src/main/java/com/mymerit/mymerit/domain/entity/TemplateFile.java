dbabb881f9dabf649be6ca3055091a3a552b5f07
Daniel Chojnicki <djchojnicki@gmail.com>
2024-05-26 21:45:51 +0200

    refactor for easier maintain on front

--- src/main/java/com/mymerit/mymerit/domain/entity/TemplateFile.java
+++ src/main/java/com/mymerit/mymerit/domain/entity/TemplateFile.java
@@ -1,9 +1,11 @@
 package com.mymerit.mymerit.domain.entity;
 
+import com.mymerit.mymerit.domain.models.ProgrammingLanguage;
 import lombok.Data;
 
 @Data
 public class TemplateFile {
+    private ProgrammingLanguage language;
     private String name;
     private String contentBase64;
 }
